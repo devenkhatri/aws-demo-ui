2c4f5bfd726882bdb654d20cc2b2452042b365b5
Deven Goratela <deven.khatri@gmail.com>
2022-03-08 16:52:19 +0530

    changed to svg favicon

--- components/page-header.js
+++ components/page-header.js
@@ -53,33 +53,34 @@ const PageHeader = ({ title, brandname }) => {
         <header className="text-gray-600 body-font border-b">
             <Head>
                 <title>{title}</title>
-                <link rel="icon" href="/favicon.ico" />
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any"></link>
             </Head>
 
-            <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8">
-                <div class="relative flex items-center justify-between">
+            <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8">
+                <div className="relative flex items-center justify-between">
                     <a
                         href="/"
                         aria-label={brandname}
                         title={brandname}
-                        class="inline-flex items-center"
+                        className="inline-flex items-center"
                     >
                         <Logo />
-                        <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                             {brandname}
                         </span>
                     </a>
-                    <ul class="flex items-center hidden space-x-8 lg:flex">
+                    <ul className="flex items-center hidden space-x-8 lg:flex">
                         <NavLinks />
                     </ul>
-                    <div class="lg:hidden">
+                    <div className="lg:hidden">
                         <button
                             aria-label="Open Menu"
                             title="Open Menu"
-                            class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
                             onClick={() => setIsMenuOpen(true)}
                         >
-                            <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                 <path
                                     fill="currentColor"
                                     d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -95,18 +96,18 @@ const PageHeader = ({ title, brandname }) => {
                             </svg>
                         </button>
                         {isMenuOpen && (
-                            <div class="absolute top-0 left-0 w-full z-50">
-                                <div class="p-5 bg-white border rounded shadow-sm">
-                                    <div class="flex items-center justify-between mb-4">
+                            <div className="absolute top-0 left-0 w-full z-50">
+                                <div className="p-5 bg-white border rounded shadow-sm">
+                                    <div className="flex items-center justify-between mb-4">
                                         <div>
                                             <a
                                                 href="/"
                                                 aria-label="Company"
                                                 title="Company"
-                                                class="inline-flex items-center"
+                                                className="inline-flex items-center"
                                             >
                                                 <Logo />
-                                                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                                                <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                                                     {brandname}
                                                 </span>
                                             </a>
@@ -115,10 +116,10 @@ const PageHeader = ({ title, brandname }) => {
                                             <button
                                                 aria-label="Close Menu"
                                                 title="Close Menu"
-                                                class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                                                 onClick={() => setIsMenuOpen(false)}
                                             >
-                                                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                                <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                                     <path
                                                         fill="currentColor"
                                                         d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -128,7 +129,7 @@ const PageHeader = ({ title, brandname }) => {
                                         </div>
                                     </div>
                                     <nav>
-                                        <ul class="space-y-4">
+                                        <ul className="space-y-4">
                                             <NavLinks />
                                         </ul>
                                     </nav>
